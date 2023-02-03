873fbf017680f04911906a51f929ca0a318687b9
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-02-03 18:48:49 +0000

    More files
    - Added Materials folder
    - Added Script Test Area Scene

---
+++ Assets/Scripts/PlayParticleRandom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticleRandom : MonoBehaviour
+{
+    public float MaxTime; // Maximum amount of time before a particle effect is played
+    public float MinTime; // Minimum amount of time before a particle effect is played
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
