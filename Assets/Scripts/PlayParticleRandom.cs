495285f6b21cf02f14b2583e6900bfd9205a5c11
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-02-03 21:07:15 +0000

    Added Scripts
    - Rotate game object on selected axis
    - Play particle effect on random timer
    - Destroy an gameobject and instantiate a new one in it's place

--- Assets/Scripts/PlayParticleRandom.cs
+++ Assets/Scripts/PlayParticleRandom.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class PlayParticleRandom : MonoBehaviour
 {
+    public ParticleSystem particles;
     public float MaxTime; // Maximum amount of time before a particle effect is played
     public float MinTime; // Minimum amount of time before a particle effect is played
+    public float Timer; // Time till next burst
 
     void Start()
     {
-        
+        Timer = Random.Range(MinTime, MaxTime);
     }
 
     void Update()
     {
-        
+        Timer -= Time.deltaTime;
+        if(Timer <= 0)
+        {
+            fireParticles();
+        }
+    }
+
+    private void fireParticles()
+    {
+        particles.Play();
+        Timer = Random.Range(MinTime, MaxTime);
     }
 }
