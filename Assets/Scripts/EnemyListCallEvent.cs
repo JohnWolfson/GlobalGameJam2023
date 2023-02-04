1f738c6c5f58ad3f3e0d4ff0fbb94f6b966780ef
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-02-04 18:22:17 +0000

    Added Scripts
    - Added Scene Change script with a public function to load a new scene
    - Added AI list check script to fire an event

---
+++ Assets/Scripts/EnemyListCallEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EnemyListCallEvent : MonoBehaviour
+{
+    public List<GameObject> NPCS = new List<GameObject>();
+    public UnityEvent Event;
+
+    void Start()
+    {
+        InvokeRepeating("checkList", 0f, 5f);
+    }
+
+    private void checkList()
+    {
+        List<GameObject> temp = new List<GameObject>();
+        foreach(GameObject NPC in NPCS)
+            if(NPC != null)
+                temp.Add(NPC);
+        NPCS = temp;
+        if (NPCS.Count < 1)
+            callEvent();
+    }
+
+    private void callEvent()
+    {
+        if(Event != null)
+            Event.Invoke();
+        Destroy(this);
+    }
+}
