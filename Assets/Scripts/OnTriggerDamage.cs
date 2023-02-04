9c486b62878df488428739ea8e7cbd886dfa8f38
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-02-04 20:08:23 +0000

    Added Script
    - Added on trigger enter damage script

---
+++ Assets/Scripts/OnTriggerDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerDamage : MonoBehaviour
+{
+    public int Damage;
+    public float DamageTimer;
+    private float timer;
+
+    void Start()
+    {
+        timer = DamageTimer;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            DamageTimer -= Time.deltaTime;
+            if(DamageTimer < 0)
+            {
+                DamageTimer = timer;
+                other.gameObject.GetComponent<CharacterStats>().TakeDamage(Damage);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            DamageTimer = 0;
+        }
+    }
+}
