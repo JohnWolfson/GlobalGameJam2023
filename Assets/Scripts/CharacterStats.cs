df9d0a05eba340ea32a14dd55e04cafccad95f75
Istalri Skolir Productions <johnthewolfson@gmail.com>
2023-02-04 00:33:43 +0000

    Player Changes
    - Added placeholder player animations
    - Added player shooting ability
    - Player accuracy added

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -5,32 +5,27 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    public Animator animator;
     public ParticleSystem particles; // Particle System use for firing
     public int MaxHP; // Player max health
     public int HP; // Player current health
     public int Ammo; // Player ammo count; 
     public int Damage; // Player damage per shot
-    public float ShootSpeed; // Time between each of the players shots
-    float timer; // Reset variable for the shoot speed
-
-    int number = 0;
+    public float Accuracy; // The higher this is, the lower the accuracy
+    public bool CanFire; // If the player has finished the current firing animation to fire again
 
     void Start()
     {
-        timer = ShootSpeed; 
+        CanFire = true;
     }
 
     void Update()
     {
-        if (Input.GetMouseButton(0) && Ammo > 0)
+        if (Input.GetMouseButtonDown(0) && Ammo > 0 && CanFire)
         {
-            if (ShootSpeed <= 0)
-                fireWeapon();
-            else
-                ShootSpeed -= Time.deltaTime;
+            CanFire = false;
+            animator.SetTrigger("Fire");
         }
-        else if (Input.GetMouseButtonUp(0))
-            ShootSpeed = 0;
     }
 
     private void fireWeapon()
@@ -38,29 +33,46 @@ public class CharacterStats : MonoBehaviour
         int layerMask = 1 << 8;
         layerMask = ~layerMask;
         RaycastHit hit;
+        Vector3 spreadVector = getSpreadVector();
+        Debug.DrawRay(transform.position, spreadVector * 100, Color.red, 600);
+        if (Physics.Raycast(transform.position, spreadVector, out hit, 10000f, layerMask))
         //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10000f, layerMask))
-        //{
-        //    switch (hit.transform.tag)
-        //    {
-        //        case "AIBomber":
-        //            //hit.transform.gameObject.GetComponent<AIBomber>().TakeDamage(damage);
-        //            break;
-        //        case "AIGunner":
-        //            //hit.transform.gameObject.GetComponent<AIGunner>().TakeDamage(damage);
-        //            break;
-        //        case "AIShield":
-        //            //hit.transform.gameObject.GetComponent<AIShield>().TakeDamage(damage);
-        //            break;
-        //        case "AIMortar":
-        //            //hit.transform.gameObject.GetComponent<AIMortar>().TakeDamage(damage);
-        //            break;
-        //        default:
-        //            Debug.Log(this.gameObject.name + ": CharacterStats -> Raycast hit returned unrecognised tag: " + hit.transform.tag);
-        //            break;
-        //    }
-        //}
-        Debug.Log("Fire " + number);
-        number += 1;
+        {
+            switch (hit.transform.tag)
+            {
+                case "AIBomber":
+                    hit.transform.gameObject.GetComponent<AIBomber>().TakeDamage(Damage);
+                    break;
+                case "AIGunner":
+                    hit.transform.gameObject.GetComponent<AIGunner>().TakeDamage(Damage);
+                    break;
+                case "AIShield":
+                    hit.transform.gameObject.GetComponent<AIShield>().TakeDamage(Damage);
+                    break;
+                case "AIMortar":
+                    hit.transform.gameObject.GetComponent<AIMortor>().TakeDamage(Damage);
+                    break;
+                default:
+                    Debug.Log(this.gameObject.name + ": CharacterStats -> Raycast hit returned unrecognised tag: " + hit.transform.tag);
+                    break;
+            }
+        }
+    }
+
+    private Vector3 getSpreadVector()
+    {
+        float yAngle = Random.Range(-Accuracy, Accuracy);
+        float xAngle = Random.Range(-Accuracy, Accuracy);
+        Quaternion ySpreadAngle = Quaternion.AngleAxis(yAngle, new Vector3(0, 1, 0));
+        Quaternion xSpreadAngle = Quaternion.AngleAxis(xAngle, new Vector3(1, 0, 0));
+        Quaternion totalSpreadAngle = xSpreadAngle * ySpreadAngle;
+        Vector3 newVector = totalSpreadAngle * transform.TransformDirection(Vector3.forward);
+        return newVector;
+    }
+
+    private void SetCanFire()
+    {
+        CanFire = true;
     }
 
     public void TakeDamage(int damage)
