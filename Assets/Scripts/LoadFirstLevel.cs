4bfa8db4d8e5e049e3a191d108fbefb245e6e226
Willomena <willdicko97@yahoo.co.uk>
2023-02-05 17:16:28 +0000

    Added FADES

---
+++ Assets/Scripts/LoadFirstLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadFirstLevel : MonoBehaviour
+{
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+    }
+}
