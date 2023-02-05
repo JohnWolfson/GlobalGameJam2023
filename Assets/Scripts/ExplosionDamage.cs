8628523eb53600b8a723aa2fe1d08e8726751cc9
Willomena <willdicko97@yahoo.co.uk>
2023-02-05 16:07:02 +0000

    Level Finalised

--- Assets/Scripts/ExplosionDamage.cs
+++ Assets/Scripts/ExplosionDamage.cs
@@ -14,6 +14,24 @@ public class ExplosionDamage : MonoBehaviour
     {
         if(other.tag == "Player"){
             other.transform.gameObject.GetComponent<CharacterStats>().TakeDamage(damage);
+            switch (other.transform.tag)
+            {
+                case "AIBomber":
+                    other.transform.gameObject.GetComponent<AIBomber>().TakeDamage(damage);
+                    break;
+                case "AIGunner":
+                    other.transform.gameObject.GetComponent<AIGunner>().TakeDamage(damage);
+                    break;
+                case "AIShield":
+                    other.transform.gameObject.GetComponent<AIShield>().TakeDamage(damage);
+                    break;
+                case "AIMortar":
+                    other.transform.gameObject.GetComponent<AIMortor>().TakeDamage(damage);
+                    break;
+                default:
+                    Debug.Log(this.gameObject.name + "' explosion hit " + other.transform.tag);
+                    break;
+            }
         }
     }
 }
---
+++ Assets/playAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayAnimation()
+    {
+        var anim = gameObject.GetComponent<Animation>();
+        anim.Play();
+    }
+}
